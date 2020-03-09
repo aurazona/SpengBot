b9474c4cd0144810c4d381cd62dc71271a3a02e9
Aurazona <60884102+aurazona@users.noreply.github.com>
2020-03-09 09:50:05 +0000

    Add files via upload
    updated files

---
+++ SpengBot/SpengBot/MyCommands.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using DiscAura;
+using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+
+namespace SpengBot
+{
+    class MyCommands
+    {
+        /***
+         * notes:
+         * ``` allows for code formatting, best for multiline responses i.e command lists
+         ***/
+        [Command("hi")]
+        public async Task Hi(CommandContext ctx)
+        {
+            if (Program.SpengDebug == false){
+                await ctx.RespondAsync($"🤙 heya, {ctx.User.Mention}!");
+            }
+            else
+            {
+                await ctx.RespondAsync("Wait a second, I'm currently being tested/debugged.");
+            }
+        }
+        [Command("random")]
+        public async Task Random(CommandContext ctx, int min, int max)
+        {
+            if (Program.SpengDebug == false)
+            {
+                var rnd = new Random();
+                await ctx.RespondAsync($"🎲 You rolled a: {rnd.Next(min, max)}");
+            }
+            else
+            {
+                await ctx.RespondAsync("Wait a second, I'm currently being tested/debugged.");
+            }
+        }
+        [Command("gay")]
+        public async Task Gay(CommandContext ctx)
+        {
+            if (Program.SpengDebug == false)
+            {
+                await ctx.RespondAsync($"no u {ctx.User.Mention}");
+            }
+            else
+            {
+                await ctx.RespondAsync("Wait a second, I'm currently being tested/debugged.");
+            }
+        }
+        [Command("fuckyou")]
+
+        public async Task FuckYou(CommandContext ctx)
+        {
+            if (Program.SpengDebug == false)
+            {
+                await ctx.RespondAsync($"speng");
+            }
+            else
+            {
+                await ctx.RespondAsync("Wait a second, I'm currently being tested/debugged.");
+            }
+        }
+        [Command("helpme")]
+
+        public async Task Helpme(CommandContext ctx)
+        {
+            if (Program.SpengDebug == false)
+            {
+                string[] lines = System.IO.File.ReadAllLines(@"X:\Data\SpengBot\davidsays.txt");
+                int davidLines = System.IO.File.ReadAllLines(@"X:\Data\SpengBot\davidsays.txt").Length; //grabs how many lines are in the file. Refer to github issue #9 for info.
+                var davidRnd = new Random();
+                int davidChoice = davidRnd.Next(0, davidLines);
+                string davidSays = lines[davidChoice];
+                await ctx.RespondAsync($"David says... {davidSays}");
+            }
+            else
+            {
+                await ctx.RespondAsync("Wait a second, I'm currently being tested/debugged.");
+            }
+        }
+        [Command("bigboy")]
+
+        public async Task bigBoy(CommandContext ctx)
+        {
+            if (Program.SpengDebug == false)
+            {
+                await ctx.RespondWithFileAsync(@"X:\Data\SpengBot\jim.PNG");
+            }
+            else
+            {
+                await ctx.RespondAsync("Wait a second, I'm currently being tested/debugged.");
+            }
+        }
+        [Command("unityhelp")]
+
+        public async Task unityHelp(CommandContext ctx)
+        {
+            if (Program.SpengDebug == false)
+            {
+                await ctx.RespondWithFileAsync(@"X:\Data\SpengBot\unity.mp3");
+            }
+            else
+            {
+                await ctx.RespondAsync("Wait a second, I'm currently being tested/debugged.");
+            }
+        }
+        [Command("urmarge")]
+
+        public async Task UrMarge(CommandContext ctx)
+        {
+            if (Program.SpengDebug == false)
+            {
+                await ctx.RespondAsync($"https://68.media.tumblr.com/f81fe91c31732e2d249ec868ae509243/tumblr_o339cnuVFp1uaabd8o1_500.png");
+
+            }
+            else
+            {
+                await ctx.RespondAsync("Wait a second, I'm currently being tested/debugged.");
+            }
+        }
+        [Command("harrywarland")]
+
+        public async Task HarryWarland(CommandContext ctx)
+        {
+            if (Program.SpengDebug == false)
+            {
+                await ctx.RespondAsync($"Harry is a nonce.");
+
+            }
+            else
+            {
+                await ctx.RespondAsync("Wait a second, I'm currently being tested/debugged.");
+            }
+        }
+        [Command("ddos")]
+
+        public async Task Ddos(CommandContext ctx)
+        {
+            if (Program.SpengDebug == false)
+            {
+                string[] lines2 = System.IO.File.ReadAllLines(@"X:\Data\SpengBot\ddos.txt");
+                var DdosRnd = new Random();
+                int DdosChoice = DdosRnd.Next(0, 17);
+                string Ddos = lines2[DdosChoice];
+                await ctx.RespondAsync($"shut up i'll ddos {Ddos}");
+            }
+            else
+            {
+                await ctx.RespondAsync("Wait a second, I'm currently being tested/debugged.");
+            }
+        }
+        [Command("stats")]
+        /***
+         * Stats:
+         * Ping
+         * Connected Guilds
+         * Users in current guild
+         * time of stat call
+         * DSharp library version
+         * guild voice region
+         * guild creation date
+         * ***/
+        public async Task Stats(CommandContext ctx) //work on this
+        {
+            if (Program.SpengDebug == false)
+            {
+                Program.StatsGrabbed = true;
+                while (Program.StatsGrabbed == true)
+                {
+                    await ctx.RespondAsync("here you go you fucking nerd");
+                    await ctx.RespondAsync($"``` Ping: {Program.SpengPing} \n Number of connected guilds: {Program.SpengGuilds} \n Number of users in current guild: {Program.SpengUsers} \n Current DSharp library version: {Program.DsharpVer} \n Guild joined: {Program.GuildMadeTime} \n Guild region: {Program.SpengRegion} \n Time of stat call: {Program.SpengTime}```");
+                    Program.StatsGrabbed = false;
+                }
+            }
+            else
+            {
+                await ctx.RespondAsync("Wait a second, I'm currently being tested/debugged.");
+            }
+            
+        }
+    }
+}
---
+++ SpengBot/SpengBot/Program.cs
@@ -0,0 +1,209 @@
+﻿using DSharpPlus;
+using System;
+using System.Threading.Tasks;
+using DSharpPlus.CommandsNext;
+using SpengBot;
+using DSharpPlus.Entities;
+
+namespace DiscAura
+{
+    class Program
+    {
+        static DiscordClient discord; //important bot stuff
+        static CommandsNextModule commands; //more important bot stuff
+        static string AuraID = ("232162231465410560"); //used to verify my identity for any admin commands
+        static ulong ShutdownAttemptConvert; //deprecated, will be removed soon-ish
+        static string virusGay; //used to verify an identity
+        static bool virusGayEnabled = false;
+        static public int SpengPing; //stores the current ping in ms
+        static public string DsharpVer; //stores the current DSharp library version
+        static public string SpengTime; //stores the current time
+        static public string SpengGuilds; //stores the amount of connected guilds
+        static public int SpengUsers; //stores the amount of users in the guild
+        static public string GuildMadeTime; //stores when spengbot joined the guild
+        static public string SpengRegion; //stores the region of the guild
+        static public bool StatsGrabbed; //used to verify that stats have, in fact, been grabbed
+        static public bool SpengDebug; //used to verify that debug mode is on/off
+        static string AdminID = "232162231465410560";
+
+        static void Main(string[] args)
+        {
+            MainAsync(args).ConfigureAwait(false).GetAwaiter().GetResult();
+        }
+
+        static async Task MainAsync(string[] args)
+        {
+            discord = new DiscordClient(new DiscordConfiguration
+            {
+                Token = System.IO.File.ReadAllText(@"X:\Data\SpengBot\token.txt"),
+                TokenType = TokenType.Bot,
+                UseInternalLogHandler = true,
+                LogLevel = LogLevel.Debug
+            });
+
+            commands = discord.UseCommandsNext(new CommandsNextConfiguration
+            {
+                StringPrefix = "."
+            });
+
+            commands.RegisterCommands<MyCommands>();
+
+            discord.MessageCreated += async e =>
+            {
+                if (e.Message.Content.ToLower().StartsWith("ping"))
+                    await e.Message.RespondAsync("pong!");
+            };
+
+            discord.MessageCreated += async e =>
+            {
+                if (e.Message.Content.ToLower().StartsWith("spengbot.shutdown"))
+                {
+                    AuraID = e.Author.Id.ToString();
+                    if (AuraID == (AdminID))
+                    {
+                        await e.Message.RespondAsync("EMERGENCY SHUTDOWN INITIATED");
+                        Environment.Exit(-1);
+                    }
+                    else
+                    {
+                        await e.Message.RespondAsync("Nice try, but last I checked you weren't Aurazona.");
+                    }
+
+                }
+            };
+
+            discord.MessageCreated += async e => //harry
+            {
+                virusGay = e.Author.Id.ToString();
+                if (virusGay == ("548944800762560533")) //ID changed
+                {
+                    if (virusGayEnabled == true)
+                    {
+                        await e.Message.RespondAsync("nonce");
+                    }
+                }
+            };
+
+            discord.MessageCreated += async e => //josh
+            {
+                virusGay = e.Author.Id.ToString();
+                if (virusGay == ("342697919201280000")) //id changed
+                {
+                    if (virusGayEnabled == true)
+                    {
+                        await e.Message.RespondAsync("shush");
+                    }
+                }
+            };
+
+            discord.MessageCreated += async e => //jacob
+            {
+                virusGay = e.Author.Id.ToString();
+                if (virusGay == ("450390645723365387")) //id changed
+                {
+                    if (virusGayEnabled == true)
+                    {
+                        await e.Message.RespondAsync("mcdonalds headass");
+                    }
+                }
+            };
+
+            discord.MessageCreated += async e => //mahdi
+            {
+                virusGay = e.Author.Id.ToString();
+                if (virusGay == ("474276732711469062")) //id changed
+                {
+                    if (virusGayEnabled == true)
+                    {
+                        await e.Message.RespondAsync("your marj");
+                    }
+                }
+            };
+
+            discord.MessageCreated += async e =>
+            {
+                if (e.Message.Content.ToLower().StartsWith("terrabot.spengrespond"))
+                {
+                    AuraID = e.Author.Id.ToString();
+                    if ((AuraID == ("232162231465410560")))
+                    {
+                        if (virusGayEnabled == true)
+                        {
+                            await e.Message.RespondAsync("Stopped responding to spengs.");
+                            virusGayEnabled = false;
+                        }
+                        else if (virusGayEnabled == false)
+                        {
+                            await e.Message.RespondAsync("Now responding to spengs.");
+                            virusGayEnabled = true;
+                        }
+                    }
+                }
+
+            };
+
+            //stat grabber part 1
+            //gets ping, DSharp version, the current time and the amount of guilds SpengBot's in.
+            static void StatGrab()
+            {
+                SpengPing = discord.Ping;
+                DsharpVer = discord.VersionString;
+                SpengTime = DateTime.Now.ToString("h:mm:ss tt");
+                SpengGuilds = discord.Guilds.Count.ToString();
+                
+            }
+
+            //stat grabber part 2
+            //grabs the member count, the date that spengbot joined the server, the region of the server and calls part 1
+            discord.MessageCreated += async e =>
+            {
+                if (e.Message.Content.ToLower().Equals(".stats"))
+                {
+                    SpengUsers = e.Guild.MemberCount;
+                    GuildMadeTime = e.Guild.JoinedAt.ToString();
+                    SpengRegion = e.Guild.RegionId;
+                    StatGrab();
+                    StatsGrabbed = true;
+                }
+            };
+
+            //debug mode
+            //disables commands
+            discord.MessageCreated += async e =>
+            {
+                if (e.Message.Content.ToLower().Equals("spengbot.debug"))
+                {
+                    if (e.Message.Author.Id.ToString() == AdminID)
+                    {
+                        if (SpengDebug == false)
+                        {
+                            await e.Message.RespondAsync("Debug mode activated.");
+                            SpengDebug = true;
+                        }
+
+                        else if (SpengDebug == true)
+                        {
+                            await e.Message.RespondAsync("Debug mode deactivated.");
+                            SpengDebug = false;
+                        }
+                    }
+                }
+            };
+
+            //ban canceller
+            //in case I get banned, automatically unbans me and shoots me a fresh invite
+            /***discord.GuildBanAdded += async e =>
+            {
+                if (e.Member.Id.ToString().Equals(AdminID))
+                {
+                    e.Guild.UnbanMemberAsync == (Aurazona#1337)
+                }
+            };
+            ***/
+            await discord.ConnectAsync();
+
+            await Task.Delay(-1);
+            
+        }
+    }
+}
